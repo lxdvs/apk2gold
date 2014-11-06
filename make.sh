8c05f5a65f2daca61860184f8df813210b08436c
Adrian Pascu <mail@adipascu.ro>
2014-11-06 20:55:55 +0200

    update dex2jarmigrate to jd-cmd from jd-cli to 0.0.9.15

--- make.sh
+++ make.sh
@@ -2,24 +2,8 @@
 SYS=`uname`
 ARCH=`uname -m`
 git submodule update --init
-cd jd-cli
-if [[ "$SYS" = "Darwin" ]]
-then
-	if [[ "$ARCH" = "x86_64" ]]
-	then
-		make osx64
-	else
-		make osx32
-	fi
-	ln -s ./osx/apktool apktool
-elif [[ "$SYS" = "Linux" ]]
-then
-	if [[ "$ARCH" = "x86_64" ]]
-	then
-		make linux64
-	else
-		make linux32
-	fi
-fi
+cd jd-cmd
+mvn clean package
+cp jd-cli/target/jd-cli.jar .. 
 cd ..
 pwd
